81175df361d16e4ad86b85119d980ccd971de74e
Bracka1337 <d3adselium@gmail.com>
2024-06-05 20:29:00 +0300

    adfgarg

--- backend/app/Models/Game_ds.php
+++ backend/app/Models/Game_ds.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Game_Ds extends Model
+class Game_ds extends Model
 {
     use HasFactory;
 
